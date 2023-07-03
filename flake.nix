e0ad46f2d865829e4dd313dd89eaf4b92a817457
Torsten Schmits <git@tryp.io>
2023-07-03 16:49:44 +0200

    flake input

--- flake.nix
+++ flake.nix
@@ -2,22 +2,17 @@
   description = "A Haskell project";
 
   inputs.hix.url = "github:tek/hix?ref=0.5.6";
+  inputs.polysemy-http.url = "github:tek/polysemy-http";
 
-  outputs = {hix, ...}: hix.lib.flake {
+  outputs = {hix, polysemy-http, ...}: hix.lib.flake {
     hackage.versionFile = "ops/version.nix";
+    depsFull = [polysemy-http];
 
     cabal = {
       license = "BSD-2-Clause-Patent";
       license-file = "LICENSE";
       author = "Author";
       ghc-options = ["-Wall"];
-    };
-
-    overrides = {hackage, unbreak, ...}: {
-      exon = unbreak;
-      polysemy-resume = unbreak;
-      polysemy = hackage "1.9.1.0" "05mhzjz6hz0dnxsn3cc0l6yyj5ch35gn8xfnx0a1gn3q8yljfg2a";
-      flatparse = hackage "0.4.1.0" "0z9q5qb3yz4phvj1wq06dld745m98yk4gvkmj1vkq7hda0mn182a";
     };
 
     packages.po-http-example = {
